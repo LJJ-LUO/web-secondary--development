bbc8b38785eb22c54b32591665b6ff835af5ba6b
jiangsixian <jiangsixian@njsdata.com>
2022-08-10 14:40:42 +0800

    feat：提交切换按钮功能

--- src/components/msgCompConfig.js
+++ src/components/msgCompConfig.js
@@ -1,11 +1,11 @@
 // 组件可派发事件
 export const events = [
   {
-    key: "onImgClick",
-    name: "图片点击",
+    key: "valueChange",
+    name: "值改变",
     payload: [
       {
-        name: "内容",
+        name: "值",
         key: "value",
         dataType: "string",
       },
@@ -15,10 +15,8 @@ export const events = [
 
 // 组件可接收事件
 export const actions = [
-  {
-    key: "messageSuccess",
-    name: "成功提示",
-  },
+
+
 ];
 
 export default {
