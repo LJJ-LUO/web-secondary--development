fbd057b9084bdb7a38c0c76401f46504b257ba71
jiangsixian <jiangsixian@njsdata.com>
2022-08-23 16:38:45 +0800

    init

--- packages/datasource-plugin/src/App.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-import "./app.less";
-import AddDataSource from "./addDatasource"
-
-
-function App(props){
-return <AddDataSource {...props}/>
-}
-export default App;
