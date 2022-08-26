a9c5115cec84c93a417ed48a00bc4861601447e0
jiangsixian <jiangsixian@njsdata.com>
2022-08-26 12:42:08 +0800

    更新老版本react模板

--- packages/old-app-plugin/scripts/genPlugin.js
+++ packages/old-app-plugin/scripts/genPlugin.js
@@ -1,9 +1,30 @@
 #!/usr/bin/env node
 
 const path = require("path");
-const fs = require("fs-extra");
-const glob = require("glob");
-const AdmZip = require("adm-zip");
+var fs = require("fs-extra");
+var glob = require("glob");
+var AdmZip = require("adm-zip");
+
+/**
+ * str 应该是  xxx_1, xxx_2 的这种格式
+ * @param {} str
+ */
+function versionAddOne(str, prefix) {
+  let items = str.split("_");
+  let version = parseInt(items[items.length - 1]);
+
+  if (isNaN(version)) {
+    version = 0;
+  }
+
+  items[items.length - 1] = version++;
+
+  if (prefix) {
+    return `${prefix}_${version}`;
+  } else {
+    return items.join("_");
+  }
+}
 
 function printZip(zip) {
   let zipEntries = zip.getEntries(); // an array of ZipEntry records
@@ -11,7 +32,15 @@ function printZip(zip) {
     console.log(zipEntry.name || zipEntry.entryName); // outputs zip entries information
   });
 }
-let configJson = require("../pluginTemp/config.json");
+
+function uuidv4() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
 // 删除老的文件
 glob.sync(path.resolve(__dirname, "../pluginTemp/js/*")).map(file => {
   console.log("file:", file);
@@ -19,21 +48,21 @@ glob.sync(path.resolve(__dirname, "../pluginTemp/js/*")).map(file => {
 });
 console.log("老文件已删除");
 
-// copy js
-
-let files = glob.sync(path.resolve(__dirname, "../dist/js/app.*.js"));
+let files = glob.sync(path.resolve(__dirname, "../build/static/js/main.*.js"));
 let jsScript = files[0];
-let mainFileName = path.basename(jsScript);
+console.log(jsScript);
+let fileName = path.basename(jsScript);
 
-// copy 静态文件
-fs.copySync(
-  jsScript,
-  path.resolve(__dirname, `../pluginTemp/js/${mainFileName}`)
-);
+fs.copySync(jsScript, path.resolve(__dirname, `../pluginTemp/js/${fileName}`));
 console.log("新文件拷贝完成");
 
-configJson.main = mainFileName;
-configJson.js_script = mainFileName;
+let configJson = require("../pluginTemp/config.json");
+let packageJson = require("../package.json");
+
+configJson.code = uuidv4();
+configJson.name = versionAddOne(configJson.name, packageJson.name);
+configJson.js_script = fileName;
+
 fs.writeFileSync(
   path.resolve(__dirname, "../pluginTemp/config.json"),
   JSON.stringify(configJson, null, 2),
@@ -51,6 +80,6 @@ let pluginPath = path.resolve(
   `../plugin-${new Date().getTime()}.zip`
 );
 zip.writeZip(pluginPath);
-fs.writeFileSync(path.resolve(__dirname, "../temp"), pluginPath, "utf-8");
+
 printZip(zip);
 console.log("打包完成...", pluginPath);
