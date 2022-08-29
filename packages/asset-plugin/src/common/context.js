66be9235930759c9b778c8e439119fa9a479e93c
jiangsixian <jiangsixian@njsdata.com>
2022-08-29 11:10:52 +0800

    初始化

--- packages/asset-plugin/src/common/context.js
+++
@@ -1,8 +0,0 @@
-
-
-const { origin, hostname } = window.location;
-
-/**
- * api的上下文路径
- */
-export const apiContextPath = origin;
--- packages/head-plugin/proxy.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  '/api': {
-    'target': 'http://10.15.112.2:18088',
-    'changeOrigin': true,
-    'pathRewrite': { '^/api' : '' },
-  },
-}
