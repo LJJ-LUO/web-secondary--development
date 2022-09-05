450ff2fa4d5f3b9edf8ba5536a8edbbdfebc5686
拾肆 <1977953215@qq.com>
2022-09-05 11:46:15 +0800

    feat:横向斜切柱状图

--- src/App.js
+++ src/App.js
@@ -1,233 +1,287 @@
 import React, { Component } from "react";
-import { Table, Button, message } from "antd";
+import * as echarts from "echarts";
 import "./app.less";
-
-const events = [
-  {
-    key: "onClick",
-    name: "点击",
-    payload: [
-      {
-        name: "名称",
-        dataType: "string",
-        key: "name",
-      },
-    ],
-  },
-];
-
-const actions = [
-  {
-    key: "messageSuccess",
-    name: "成功提示",
-    params: [
-      {
-        key: "value",
-        name: "值",
-        dataType: "string",
-      },
-    ],
-  },
-];
+import toopImg from "../src/assets/tooipBg1.png";
 
 export default class App extends Component {
-  state = {
-    dataSource: [],
-    rowId: "",
-    columns: [],
-  };
-
   constructor(props) {
     super(props);
-    const { pubSub } = props;
-    pubSub?.subscribe &&
-      pubSub.subscribe("analyzeDataSource", data => {
-        this.loadTableFromData && this.loadTableFromData(data);
-      });
+    let external = props.options?.externalVariables ? props.options.externalVariables : {};
+
+    // 系列颜色
+    this.barBackgroundColor = external["背景颜色"] || "transparent";
+    this.barColorArr = external["系列颜色数组"] || "#3FF2FD-#F39924-#27A6FE";
+    this.barGraduallyColorArr = external["对应渐变颜色数组"] || "#F60C0D-#2EBC27-#0413FF";
+    // 图例配置
+    this.legendIconWidth = external["图例图标宽度"] || "18";
+    this.legendIconHeight = external["图例图标高度"] || "3";
+    this.legendColor = external["图例文字颜色"] || "#fff";
+    this.legendFontSize = external["图例文字字号"] || "14";
+    this.legendGap = external["图例间距"] || "20";
+    this.legendPosition = external["图例整体位置"] || "3";
+    // X轴配置
+    this.xAxisLineColor = external["X轴线颜色"] || "#12294D";
+    this.xAxisLabelColor = external["X轴文字颜色"] || "#fff";
+    this.xAxisLabelFontSize = external["X轴文字字号"] || "14";
+    // Y轴配置
+    this.yAxisLineColor = external["Y轴线颜色"] || "#fff";
+    this.yAxisLabelColor = external["Y轴文字颜色"] || "#fff";
+    this.yAxisLabelFontSize = external["Y轴文字字号"] || "14";
+    // 柱形数据配置
+    this.barDataWdith = external["柱形数据宽度"] || "35";
+    this.barDataTilt = external["柱形数据倾斜角度"] || "30";
+    this.barDataSpacing = external["柱形数据间距"] || "20";
+    this.barDataLeftPosition = external["柱形数据平移"] || "40";
+    // 悬浮窗配置
+    this.tooltipHeight = external["悬浮窗高度"] || "115";
+    this.tooltipWidth = external["悬浮窗宽度"] || "210";
   }
 
-  loadTableSource = data => {
-    let title = data[0];
-    try {
-      const columns = title.map((item, index) => ({
-        title: item,
-        dataIndex: item,
-        key: index,
-        align: "center",
-        ellipsis: true,
-      }));
-      this.setState({ columns });
-    } catch (error) {
-      console.error(error);
-    }
-    let dataSourceInit = data.slice(1);
-    let dataSource = [];
-    dataSourceInit.forEach((item, index) => {
-      let obj = {};
-      item.forEach((it, i) => {
-        obj[title[i]] = it;
-        obj.key = index;
-      });
-      dataSource.push(obj);
-    });
-    this.setState({ dataSource });
-  };
-
   componentDidMount() {
-    const { dataSource, updateProcess, componentId } = this.props;
-    const data = dataSource;
-    this.loadTableSource(data);
+    if (process.env.NODE_ENV !== "development") {
+      this.refs["jsgk_barX_tilt"].parentNode.style.width = "100%";
+      this.refs["jsgk_barX_tilt"].parentNode.style.height = "100%";
+      this.refs["jsgk_barX_tilt"].parentNode.parentNode.style.overflow = "visible";
+      this.refs["jsgk_barX_tilt"].parentNode.parentNode.parentNode.parentNode.style.overflow = "visible";
+      this.refs["jsgk_barX_tilt"].parentNode.parentNode.parentNode.parentNode.parentNode.style.overflow = "visible";
+      this.refs["jsgk_barX_tilt"].parentNode.parentNode.style.minHeight = 0;
+    }
 
-    window?.componentCenter?.register(componentId, "comp", this, {
-      events,
-      actions,
-    });
-    updateProcess && updateProcess();
+    this.handleEchartsData();
 
     this.Event_Center_getName = () => {
-      return "Demo实例";
+      return "横向柱状图斜切";
     };
   }
 
-  do_EventCenter_messageSuccess(param) {
-    console.log(param);
-    alert("动作执行成功！");
+  handleEchartsData() {
+    let propsData = JSON.parse(JSON.stringify(this.props.dataSource));
+
+    let yAxisData = [];
+    let dataList = [];
+    let seriesData = [];
+
+    propsData[0].forEach((item, index) => {
+      let obj = {
+        name: "",
+        data: [],
+      };
+      obj.name = item;
+      propsData.forEach((e, i) => {
+        if (i > 0) {
+          yAxisData.push(e[0]);
+          obj.data.push(e[index]);
+        }
+      });
+      if (index > 0) {
+        dataList.push(obj);
+      }
+    });
+
+    yAxisData = [...new Set(yAxisData)];
+
+    dataList.forEach((item, index) => {
+      seriesData.push({
+        type: "custom",
+        name: item.name,
+        animation: true,
+        renderItem: (params, api) => {
+          const location = api.coord([api.value(0), api.value(1)]);
+          const xlocation = api.coord([0, api.value(1)]);
+          return {
+            type: "InclinedRoofBar",
+            shape: {
+              api,
+              xValue: api.value(0),
+              yValue: api.value(1),
+              x: location[0],
+              y: location[1] + index * (Number(this.barDataWdith) + Number(this.barDataSpacing)) - Number(this.barDataLeftPosition),
+              xAxisPoint: [xlocation[0] + 1, xlocation[1] + index * (Number(this.barDataWdith) + Number(this.barDataSpacing)) - Number(this.barDataLeftPosition)],
+            },
+            style: {
+              fill: new echarts.graphic.LinearGradient(1, 0, 0, 0, [
+                {
+                  offset: 0,
+                  color: this.barColorArr.split("-")[index] || "#3FF2FD",
+                },
+                {
+                  offset: 1,
+                  color: this.barGraduallyColorArr.split("-")[index] || "rgba(0, 0, 0, 0)",
+                },
+              ]),
+            },
+          };
+        },
+        data: item.data,
+      });
+    });
+
+    this.initEcharts(yAxisData, seriesData);
   }
 
-  moveToTop = () => {
-    if (this.isSelected()) {
-      const { rowId, dataSource } = this.state;
-      if (rowId === 0) {
-        message.warn("已在第一行，不能再置顶！");
-      } else {
-        let data = dataSource;
-        let temporaryData = data.splice(rowId, 1)[0];
-        data.unshift(temporaryData);
-        this.setState({ dataSource: [...data], rowId: 0 });
-      }
+  initEcharts(yAxisData, seriesData) {
+    let myChart = echarts.init(this.refs["jsgk_barX_tilt"]);
+    let option = {};
+
+    // 绘制左侧面
+    const InclinedRoofBar = echarts.graphic.extendShape({
+      shape: { x: 0, y: 0, width: 10 },
+      buildPath: (ctx, shape) => {
+        const xAxisPoint = shape.xAxisPoint;
+        const c0 = [shape.x, shape.y];
+        const c1 = [shape.x + Number(this.barDataTilt), shape.y - Number(this.barDataWdith)];
+        const c2 = [xAxisPoint[0], xAxisPoint[1] - Number(this.barDataWdith)];
+        const c3 = [xAxisPoint[0], xAxisPoint[1]];
+        ctx.moveTo(c0[0], c0[1]).lineTo(c1[0], c1[1]).lineTo(c2[0], c2[1]).lineTo(c3[0], c3[1]).closePath();
+      },
+    });
+    echarts.graphic.registerShape("InclinedRoofBar", InclinedRoofBar);
+
+    option = {
+      backgroundColor: this.barBackgroundColor,
+
+      color: this.barColorArr.split("-"),
+
+      tooltip: {
+        trigger: "axis",
+        axisPointer: {
+          type: "none",
+        },
+        backgroundColor: "transparent",
+        borderWidth: 0,
+        formatter: (params) => {
+          let list = [];
+          let listItem = "";
+          for (var i = 0; i < params.length; i++) {
+            list.push(
+              `<div style="font-size: 14px; color: #fff; padding-left: 15px; padding-top: 2px;">
+                <div style="display: flex; align-items: center;">
+                  <div style="width: 18px; border: 1px solid ${params[i].color}; margin-right: 10px;">
+                  </div>${params[i].seriesName}: ${params[i].value}</div>
+                </div>
+              `
+            );
+          }
+          listItem = list.join("");
+          return (
+            `
+              <div style="background: url(${toopImg}) no-repeat; background-size: cover; background-attachment: fixed; height: ${Number(this.tooltipHeight)}px; width: ${Number(this.tooltipWidth)}px;">
+              <div style="font-size: 14px; color: #B0C4DD; padding-left: 15px; padding-top: 12px;">${params[0].name}</div>
+            ` +
+            listItem +
+            `</div>`
+          );
+        },
+      },
+
+      legend: {
+        top: `${this.legendPosition}%`,
+        right: "5%",
+        itemWidth: Number(this.legendIconWidth),
+        itemHeight: Number(this.legendIconHeight),
+        itemGap: Number(this.legendGap),
+        textStyle: {
+          color: this.legendColor,
+          fontSize: this.legendFontSize,
+        },
+      },
+
+      grid: {
+        left: "5%",
+        right: "5%",
+        bottom: "5%",
+        top: "8%",
+        containLabel: true,
+      },
+
+      xAxis: {
+        splitLine: {
+          show: true,
+          lineStyle: {
+            color: this.xAxisLineColor,
+            type: "dashed",
+          },
+        },
+        axisLine: {
+          show: false,
+        },
+        axisLabel: {
+          show: true,
+          formatter: function (params) {
+            let str = String(params);
+            if (str.length === 9) {
+              return `${params / 100000000}亿`;
+            } else if (str.length === 8) {
+              return `${params / 1000000}00万`;
+            } else if (str.length === 7) {
+              return `${params / 100000}0万`;
+            } else if (str.length === 6) {
+              return `${params / 10000}万`;
+            } else if (str.length === 5) {
+              return `${params / 10000}万`;
+            }
+            return params;
+          },
+          textStyle: {
+            color: this.xAxisLabelColor,
+            fontSize: this.xAxisLabelFontSize,
+          },
+        },
+        axisTick: {
+          show: false,
+        },
+      },
+
+      yAxis: [
+        {
+          type: "category",
+          inverse: true,
+          axisLabel: {
+            show: true,
+            textStyle: {
+              color: this.yAxisLabelColor,
+              fontSize: this.yAxisLabelFontSize,
+            },
+          },
+          splitLine: {
+            show: false,
+          },
+          axisTick: {
+            show: false,
+          },
+          axisLine: {
+            show: true,
+            lineStyle: {
+              width: 1,
+              color: this.yAxisLineColor,
+            },
+          },
+          data: yAxisData,
+        },
+      ],
+
+      series: seriesData,
+    };
+
+    option && myChart.setOption(option);
+
+    function debounce(func, ms = 1000) {
+      let timer;
+      return function (...args) {
+        if (timer) clearTimeout(timer);
+        timer = setTimeout(() => {
+          func.apply(this, args);
+        }, ms);
+      };
     }
-  };
-
-  moveUp = () => {
-    if (this.isSelected()) {
-      const { rowId, dataSource } = this.state;
-      if (rowId === 0) {
-        message.warn("已在第一行，不能再上移！");
-      } else {
-        let data = dataSource;
-        let temporaryData = data[rowId];
-        data[rowId] = data[rowId - 1];
-        data[rowId - 1] = temporaryData;
-        this.setState({ dataSource: [...data], rowId: rowId - 1 });
-      }
-    }
-  };
-
-  moveDown = () => {
-    if (this.isSelected()) {
-      const { rowId, dataSource } = this.state;
-      if (rowId === dataSource.length - 1) {
-        message.warn("已在最后一行，不能再下移！");
-      } else {
-        let data = dataSource;
-        let temporaryData = data[rowId];
-        data[rowId] = data[rowId + 1];
-        data[rowId + 1] = temporaryData;
-        this.setState({ dataSource: [...data], rowId: rowId + 1 });
-      }
-    }
-  };
-
-  moveToBottom = () => {
-    if (this.isSelected()) {
-      const { rowId, dataSource } = this.state;
-      if (rowId === dataSource.length - 1) {
-        message.warn("已在最后一行，不能再置底！");
-      } else {
-        let data = dataSource;
-        let temporaryData = data.splice(rowId, 1)[0];
-        data.push(temporaryData);
-        this.setState({ dataSource: [...data], rowId: dataSource.length - 1 });
-      }
-    }
-  };
-
-  setRowClassName = (_, index) => {
-    return index === this.state.rowId ? "selected-row" : "";
-  };
-
-  isSelected = () => {
-    const { rowId } = this.state;
-    rowId === "" && message.warn("请选择要调整的行！");
-    return rowId !== "";
-  };
+    const task = () => {
+      myChart.resize();
+    };
+    const debounceTask = debounce(task, 300);
+    window.addEventListener("resize", debounceTask);
+  }
 
   render() {
-    const { dataSource, rowId, columns } = this.state;
-    const { componentId, options = {} } = this.props;
-    const { externalVariables = {} } = options;
-    const { headerBGColor = "#0f2437" } = externalVariables;
-    return (
-      <div className="analyzer-demo" style={{ width: "100%", height: "100%" }}>
-        <div className="func-area" style={{ backgroundColor: headerBGColor }}>
-          <div
-            style={{
-              width: "100%",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "flex-end",
-              paddingRight: "20px",
-            }}
-          >
-            <label
-              style={{ color: "white", fontSize: "18px", fontWeight: "bold" }}
-            >
-              优先级调整
-            </label>
-            <Button onClick={this.moveToTop} size="small">
-              置顶
-            </Button>
-            <Button onClick={this.moveUp} size="small">
-              上移
-            </Button>
-            <Button onClick={this.moveDown} size="small">
-              下移
-            </Button>
-            <Button onClick={this.moveToBottom} size="small">
-              置底
-            </Button>
-          </div>
-        </div>
-        <Table
-          columns={columns}
-          dataSource={dataSource}
-          pagination={false}
-          size="small"
-          rowClassName={this.setRowClassName}
-          onRow={(_, index) => {
-            return {
-              onClick: () => {
-                this.setState({ rowId: rowId === index ? "" : index });
-                window?.eventCenter?.triggerEvent(componentId, "onClick", {
-                  name: "二开插件",
-                });
-              },
-            };
-          }}
-          scroll={{ y: "calc(100% - 39px)" }}
-          style={{ height: "calc(100% - 63px)" }}
-        />
-        <div
-          className="card-bg"
-          onClick={() => {
-            window?.eventCenter?.triggerEvent(componentId, "onClick", {
-              name: "二开插件",
-            });
-          }}
-        >
-          点这里
-        </div>
-      </div>
-    );
+    return <div ref="jsgk_barX_tilt" style={{ width: "100%", height: "100%" }}></div>;
   }
 }
--- src/index.js
+++ src/index.js
@@ -20,21 +20,49 @@ import App from "./App";
  */
 if (process.env.NODE_ENV !== "production") {
   require("antd/dist/antd.css");
+
+  // 资产数据
   const dataSource = [
-    ["标题一", "标题二", "标题三"],
-    ["标题一数据一", "标题二数据一", "标题三数据一"],
-    ["标题一数据二", "标题二数据二", "标题三数据二"],
-    ["标题一数据三", "标题二数据三", "标题三数据三"],
+    ["港口信息", "年度吞吐量指标", "年度外贸量指标", "年度自然量指标"],
+    ["南京港", "47.89", "34.94", "31.74"],
+    ["镇江港", "48.70", "32.12", "181.03"],
+    ["苏州港", "57.66", "31.41", "20.94"],
   ];
+
+  // 配置项
   const options = {
     externalVariables: {
-      headerBGColor: "#000",
+      // 系列颜色
+      背景颜色: "transparent", // 建议#141C34
+      系列颜色数组: "#3FF2FD-#F39924-#27A6FE",
+      对应渐变颜色数组: "#F60C0D-#2EBC27-#0413FF",
+      // 图例配置
+      图例图标宽度: "18",
+      图例图标高度: "3",
+      图例文字颜色: "#fff",
+      图例文字字号: "14",
+      图例间距: "20",
+      图例整体位置: "5",
+      // X轴配置
+      X轴线颜色: "#fff",
+      X轴文字颜色: "#fff",
+      X轴文字字号: "14",
+      // Y轴配置
+      Y轴线颜色: "#fff",
+      Y轴文字颜色: "#fff",
+      Y轴文字字号: "14",
+      // 柱形数据配置
+      柱形数据宽度: "35",
+      柱形数据倾斜角度: "30",
+      柱形数据间距: "20",
+      柱形数据平移: "40",
+      // 悬浮窗配置
+      悬浮窗高度: "115",
+      悬浮窗宽度: "210",
     },
   };
-  ReactDOM.render(
-    <App dataSource={dataSource} options={options} />,
-    document.getElementById("root")
-  );
+
+  ReactDOM.render(<App dataSource={dataSource} options={options} />, document.getElementById("root"));
 } else {
   if (!window.CUSTOM_PLUGIN) {
     window.CUSTOM_PLUGIN = new Map();
