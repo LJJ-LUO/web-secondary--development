045657191ddbf49e40360a58618c88b131763d8e
7DodgerBlue <50253073+151hudu@users.noreply.github.com>
2022-09-06 14:07:14 +0800

    2022.8.29-江苏港口数据-流量od-analyzer-react-plugin (#224)

--- src/App.js
+++ src/App.js
@@ -1,233 +1,390 @@
 import React, { Component } from "react";
-import { Table, Button, message } from "antd";
+import * as echarts from "echarts";
 import "./app.less";
-
-const events = [
-  {
-    key: "onClick",
-    name: "点击",
-    payload: [
-      {
-        name: "名称",
-        dataType: "string",
-        key: "name",
-      },
-    ],
-  },
-];
-
-const actions = [
-  {
-    key: "messageSuccess",
-    name: "成功提示",
-    params: [
-      {
-        key: "value",
-        name: "值",
-        dataType: "string",
-      },
-    ],
-  },
-];
-
+import geoJson from "./region.json";
+import chinaMapOutlineData from "./chinaMapOutlineData.json";
+import $ from "jquery";
+import backImg from "./Frame 33557.png";
 export default class App extends Component {
-  state = {
-    dataSource: [],
-    rowId: "",
-    columns: [],
-  };
+  state = {};
 
   constructor(props) {
     super(props);
-    const { pubSub } = props;
-    pubSub?.subscribe &&
-      pubSub.subscribe("analyzeDataSource", data => {
-        this.loadTableFromData && this.loadTableFromData(data);
-      });
-  }
-
-  loadTableSource = data => {
-    let title = data[0];
-    try {
-      const columns = title.map((item, index) => ({
-        title: item,
-        dataIndex: item,
-        key: index,
-        align: "center",
-        ellipsis: true,
-      }));
-      this.setState({ columns });
-    } catch (error) {
-      console.error(error);
+    console.log(props);
+    let options = JSON.parse(JSON.stringify(props.options));
+    if (props.options.externalVariables) {
+      this.外层边界线条颜色 = options.externalVariables.外层边界线条颜色;
+      this.外层边边界线条粗细 = options.externalVariables.外层边边界线条粗细;
+      this.内层边界线条颜色 = options.externalVariables.内层边界线条颜色;
+      this.内层边界线条粗细 = options.externalVariables.内层边界线条粗细;
+      this.下层边界线条颜色 = options.externalVariables.下层边界线条颜色;
+      this.地图颜色 = options.externalVariables.地图颜色;
+      this.汇聚点 = options.externalVariables.汇聚点 ? eval(options.externalVariables.汇聚点) : "";
+      this.汇聚点颜色 = options.externalVariables.汇聚点颜色;
+      this.飞线颜色 = options.externalVariables.飞线颜色;
+      this.toolTip宽度 = options.externalVariables.toolTip宽度;
+      this.toolTip高度 = options.externalVariables.toolTip高度;
+      this.toolTip文字颜色 = options.externalVariables.toolTip文字颜色;
+      this.toolTip文字大小 = options.externalVariables.toolTip文字大小;
+      this.地图文字颜色 = options.externalVariables.地图文字颜色;
     }
-    let dataSourceInit = data.slice(1);
-    let dataSource = [];
-    dataSourceInit.forEach((item, index) => {
-      let obj = {};
-      item.forEach((it, i) => {
-        obj[title[i]] = it;
-        obj.key = index;
+  }
+  initEcharts(dataa) {
+    let echartsData = [];
+    dataa.forEach((item, index) => {
+      if (index > 0) {
+        let message = {
+          coords: [],
+        };
+        dataa[0].forEach((sonItem, sonIndex) => {
+          message[sonItem] = item[sonIndex];
+        });
+        echartsData.push(message);
+      }
+    });
+    echartsData.forEach((item, index) => {
+      geoJson.features.forEach((itemGeo, indexGeo) => {
+        if (itemGeo.properties.name == item.name) {
+          item.coords.push(itemGeo.properties.center);
+        }
       });
-      dataSource.push(obj);
     });
-    this.setState({ dataSource });
-  };
-
-  componentDidMount() {
-    const { dataSource, updateProcess, componentId } = this.props;
-    const data = dataSource;
-    this.loadTableSource(data);
-
-    window?.componentCenter?.register(componentId, "comp", this, {
-      events,
-      actions,
+    echartsData.forEach((item, index) => {
+      geoJson.features.forEach((itemGeo, indexGeo) => {
+        if (itemGeo.properties.name == item.toAddress) {
+          item.coords.push(itemGeo.properties.center);
+          item.lineStyle = { color: this.飞线颜色 ? this.飞线颜色 : "#FB9825" };
+        }
+      });
     });
-    updateProcess && updateProcess();
+    let showRadio = this.汇聚点 ? this.汇聚点 : ["新疆", "江苏", "广东"];
+    let points = [];
+    showRadio.forEach((itemRadio, indexRadio) => {
+      echartsData.forEach((item, index) => {
+        if (item.name == itemRadio) {
+          let message = {
+            value: item.coords[0],
+            itemStyle: {
+              color: this.汇聚点颜色 ? this.汇聚点颜色 : "#FB9825",
+            },
+          };
+          points.push(message);
+        }
+      });
+    });
+    const myChart = echarts.init(this.refs.watchOD);
+    myChart.showLoading();
+    let index = -1;
+    let chinaMapOutline = chinaMapOutlineData;
+    echarts.registerMap("china2", geoJson);
+    echarts.registerMap("chinaMapOutline", chinaMapOutline);
+    myChart.hideLoading();
+    var data = [
+      {
+        name: "青海",
+        value: 110,
+      },
+    ];
+    let option = {
+      backgroundColor: "transparent",
+      tooltip: {
+        trigger: "item",
+        borderWidth: 0,
+        // position: 'unset',
+        padding: 0,
+        backgroundColor: "transparent",
+        formatter: (params) => {
+          var tip = "";
+          if (params.data && params.data.toAddress) {
+            tip = `<div style="position:relative;width:${this.toolTip宽度 ? this.toolTip宽度 : 100}px;height:${
+              this.toolTip高度 ? this.toolTip高度 : 40
+            }px;padding:0;display:flex;justify-content:center">
+            <div  style='position:absolute;background:url(${backImg});background-size:100% 100%;width:${this.toolTip宽度 ? this.toolTip宽度 : 138}px;height:${
+              this.toolTip高度 ? this.toolTip高度 : 40
+            }px;display:${params.data.toAddress ? "block" : "none"}'></div>
+                      <span style="position:absolute;color:red;line-height:${this.toolTip高度 ? this.toolTip高度 : 40}px;font-size:${
+              this.toolTip文字大小 ? this.toolTip文字大小 : 14
+            }px;color:${this.toolTip文字颜色 ? this.toolTip文字颜色 : "#ffffff"}">${params.data.showValue ? params.data.showValue : params.data.fromName}${params.value}</span>
+                      </div>`;
+          }
 
-    this.Event_Center_getName = () => {
-      return "Demo实例";
+          return tip;
+        },
+      },
+      geo: [
+        {
+          silent: true,
+          map: "chinaMapOutline",
+          zoom: 1.2,
+          top: "10%",
+          label: {
+            normal: {
+              show: false,
+              textStyle: {
+                color: "#fff",
+              },
+            },
+            emphasis: {
+              textStyle: {
+                color: "#fff",
+              },
+            },
+          },
+          regions: [
+            {
+              name: "南海诸岛",
+              itemStyle: {
+                // 隐藏地图
+                normal: {
+                  opacity: 0, // 为 0 时不绘制该图形
+                  show: false,
+                },
+              },
+              label: {
+                show: false, // 隐藏文字
+              },
+            },
+          ],
+          roam: false,
+          itemStyle: {
+            normal: {
+              areaColor: "rgba(0,255,255,.02)",
+              borderColor: this.下层边界线条颜色 ? this.下层边界线条颜色 : "#027DE4",
+              borderWidth: 3,
+              shadowColor: this.下层边界线条颜色 ? this.下层边界线条颜色 : "#027DE4",
+              shadowOffsetX: -2,
+              shadowOffsetY: 7,
+              shadowBlur: 0,
+            },
+            emphasis: {
+              areaColor: "transparent", //悬浮背景
+              textStyle: {
+                color: "#fff",
+              },
+            },
+          },
+        },
+        {
+          silent: true,
+          map: "china2",
+          zoom: 1.2,
+          top: "10%",
+          label: {
+            normal: {
+              show: false,
+              textStyle: {
+                color: "#fff",
+              },
+            },
+            emphasis: {
+              textStyle: {
+                color: "#fff",
+              },
+            },
+          },
+          regions: [
+            {
+              name: "南海诸岛",
+              itemStyle: {
+                // 隐藏地图
+                normal: {
+                  opacity: 0, // 为 0 时不绘制该图形
+                },
+              },
+              label: {
+                show: false, // 隐藏文字
+              },
+            },
+          ],
+          roam: false,
+          itemStyle: {
+            normal: {
+              areaColor: this.地图颜色 ? this.地图颜色 : "tranparent",
+              borderColor: this.内层边界线条颜色 ? this.内层边界线条颜色 : "#5DBDF2",
+              borderWidth: this.内层边界线条粗细 ? this.内层边界线条粗细 : "3",
+              shadowColor: "#00ffff",
+              shadowOffsetX: 0,
+              shadowOffsetY: 0,
+              shadowBlur: 0,
+            },
+            emphasis: {
+              areaColor: "transparent", //悬浮背景
+              textStyle: {
+                color: "#fff",
+              },
+            },
+          },
+        },
+        {
+          silent: true,
+          map: "chinaMapOutline",
+          zoom: 1.2,
+          top: "10%",
+          label: {
+            normal: {
+              show: false,
+              textStyle: {
+                color: "#fff",
+              },
+            },
+            emphasis: {
+              textStyle: {
+                color: "#fff",
+              },
+            },
+          },
+          regions: [
+            {
+              name: "南海诸岛",
+              itemStyle: {
+                // 隐藏地图
+                normal: {
+                  opacity: 0, // 为 0 时不绘制该图形
+                },
+              },
+              label: {
+                show: false, // 隐藏文字
+              },
+            },
+          ],
+          roam: false,
+          itemStyle: {
+            normal: {
+              areaColor: "rgba(0,255,255,.02)",
+              borderColor: this.外层边界线条颜色 ? this.外层边界线条颜色 : "#3AFFFF",
+              borderWidth: this.外层边边界线条粗细 ? this.外层边边界线条粗细 : "3",
+              shadowColor: "#00ffff",
+              shadowOffsetX: 0,
+              shadowOffsetY: 0,
+              shadowBlur: 0,
+            },
+            emphasis: {
+              areaColor: "transparent", //悬浮背景
+              textStyle: {
+                color: "#fff",
+              },
+            },
+          },
+        },
+      ],
+      series: [
+        {
+          type: "map",
+          roam: false,
+          data: echartsData,
+          label: {
+            normal: {
+              show: true,
+              textStyle: {
+                color: this.地图文字颜色 ? this.地图文字颜色 : "#FFFFFF",
+              },
+            },
+            emphasis: {
+              textStyle: {
+                color: this.地图文字颜色 ? this.地图文字颜色 : "#FFFFFF",
+                fontSize: "16",
+              },
+            },
+          },
+          itemStyle: {
+            normal: {
+              borderColor: "rgba(147, 235, 248,0)",
+              borderWidth: 1,
+              areaColor: "transparent",
+              borderWidth: 3,
+            },
+            emphasis: {
+              areaColor: "transparent",
+              borderWidth: 0.1,
+            },
+          },
+          zoom: 1.2,
+          //     roam: false,
+          map: "china2", //使用
+          // data: this.difficultData //热力图数据   不同区域 不同的底色
+        },
+        {
+          type: "effectScatter",
+          coordinateSystem: "geo",
+          showEffectOn: "render",
+          zlevel: 1,
+          rippleEffect: {
+            period: 15,
+            scale: 4,
+            brushType: "fill",
+          },
+          hoverAnimation: true,
+          label: {
+            normal: {
+              formatter: "{b}",
+              position: "right",
+              offset: [15, 0],
+              color: "#1DE9B6",
+              show: true,
+            },
+          },
+          itemStyle: {
+            normal: {
+              color: "#1DE9B6",
+              shadowBlur: 10,
+              shadowColor: "#333",
+            },
+          },
+          symbolSize: 12,
+          data: points,
+        }, //地图线的动画效果
+        {
+          type: "lines",
+          zlevel: 2,
+          effect: {
+            show: true,
+            period: 4, //箭头指向速度，值越小速度越快
+            trailLength: 0.4, //特效尾迹长度[0,1]值越大，尾迹越长重
+            symbol: "arrow", //箭头图标
+            symbolSize: 2.5, //图标大小
+          },
+          lineStyle: {
+            normal: {
+              color: "#1DE9B6",
+              width: 1, //线条宽度
+              opacity: 0.1, //尾迹线条透明度
+              curveness: 0.3, //尾迹线条曲直度
+            },
+          },
+          data: echartsData,
+        },
+      ],
     };
+    myChart.setOption(option, true);
+    myChart.setOption(option);
+    function debounce(func, ms = 1000) {
+      let timer;
+      return function (...args) {
+        if (timer) {
+          clearTimeout(timer);
+        }
+        timer = setTimeout(() => {
+          func.apply(this, args);
+        }, ms);
+      };
+    }
+    const task = () => {
+      console.log("resize");
+      myChart.resize();
+    };
+    const debounceTask = debounce(task, 1000);
+    window.addEventListener("resize", debounceTask);
   }
-
-  do_EventCenter_messageSuccess(param) {
-    console.log(param);
-    alert("动作执行成功！");
+  componentDidMount() {
+    const { dataSource } = this.props;
+    const data = JSON.parse(JSON.stringify(dataSource));
+    this.refs["watchOD"].parentNode.style.width = "100%";
+    this.refs["watchOD"].parentNode.style.height = "100%";
+    this.refs["watchOD"].parentNode.parentNode.style.minHeight = 0;
+    this.initEcharts(data);
   }
-
-  moveToTop = () => {
-    if (this.isSelected()) {
-      const { rowId, dataSource } = this.state;
-      if (rowId === 0) {
-        message.warn("已在第一行，不能再置顶！");
-      } else {
-        let data = dataSource;
-        let temporaryData = data.splice(rowId, 1)[0];
-        data.unshift(temporaryData);
-        this.setState({ dataSource: [...data], rowId: 0 });
-      }
-    }
-  };
-
-  moveUp = () => {
-    if (this.isSelected()) {
-      const { rowId, dataSource } = this.state;
-      if (rowId === 0) {
-        message.warn("已在第一行，不能再上移！");
-      } else {
-        let data = dataSource;
-        let temporaryData = data[rowId];
-        data[rowId] = data[rowId - 1];
-        data[rowId - 1] = temporaryData;
-        this.setState({ dataSource: [...data], rowId: rowId - 1 });
-      }
-    }
-  };
-
-  moveDown = () => {
-    if (this.isSelected()) {
-      const { rowId, dataSource } = this.state;
-      if (rowId === dataSource.length - 1) {
-        message.warn("已在最后一行，不能再下移！");
-      } else {
-        let data = dataSource;
-        let temporaryData = data[rowId];
-        data[rowId] = data[rowId + 1];
-        data[rowId + 1] = temporaryData;
-        this.setState({ dataSource: [...data], rowId: rowId + 1 });
-      }
-    }
-  };
-
-  moveToBottom = () => {
-    if (this.isSelected()) {
-      const { rowId, dataSource } = this.state;
-      if (rowId === dataSource.length - 1) {
-        message.warn("已在最后一行，不能再置底！");
-      } else {
-        let data = dataSource;
-        let temporaryData = data.splice(rowId, 1)[0];
-        data.push(temporaryData);
-        this.setState({ dataSource: [...data], rowId: dataSource.length - 1 });
-      }
-    }
-  };
-
-  setRowClassName = (_, index) => {
-    return index === this.state.rowId ? "selected-row" : "";
-  };
-
-  isSelected = () => {
-    const { rowId } = this.state;
-    rowId === "" && message.warn("请选择要调整的行！");
-    return rowId !== "";
-  };
 
   render() {
-    const { dataSource, rowId, columns } = this.state;
-    const { componentId, options = {} } = this.props;
-    const { externalVariables = {} } = options;
-    const { headerBGColor = "#0f2437" } = externalVariables;
-    return (
-      <div className="analyzer-demo" style={{ width: "100%", height: "100%" }}>
-        <div className="func-area" style={{ backgroundColor: headerBGColor }}>
-          <div
-            style={{
-              width: "100%",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "flex-end",
-              paddingRight: "20px",
-            }}
-          >
-            <label
-              style={{ color: "white", fontSize: "18px", fontWeight: "bold" }}
-            >
-              优先级调整
-            </label>
-            <Button onClick={this.moveToTop} size="small">
-              置顶
-            </Button>
-            <Button onClick={this.moveUp} size="small">
-              上移
-            </Button>
-            <Button onClick={this.moveDown} size="small">
-              下移
-            </Button>
-            <Button onClick={this.moveToBottom} size="small">
-              置底
-            </Button>
-          </div>
-        </div>
-        <Table
-          columns={columns}
-          dataSource={dataSource}
-          pagination={false}
-          size="small"
-          rowClassName={this.setRowClassName}
-          onRow={(_, index) => {
-            return {
-              onClick: () => {
-                this.setState({ rowId: rowId === index ? "" : index });
-                window?.eventCenter?.triggerEvent(componentId, "onClick", {
-                  name: "二开插件",
-                });
-              },
-            };
-          }}
-          scroll={{ y: "calc(100% - 39px)" }}
-          style={{ height: "calc(100% - 63px)" }}
-        />
-        <div
-          className="card-bg"
-          onClick={() => {
-            window?.eventCenter?.triggerEvent(componentId, "onClick", {
-              name: "二开插件",
-            });
-          }}
-        >
-          点这里
-        </div>
-      </div>
-    );
+    return <div ref="watchOD" style={{ width: "100%", height: "100%" }}></div>;
   }
 }
--- src/index.js
+++ src/index.js
@@ -20,21 +20,28 @@ import App from "./App";
  */
 if (process.env.NODE_ENV !== "production") {
   require("antd/dist/antd.css");
-  const dataSource = [
-    ["标题一", "标题二", "标题三"],
-    ["标题一数据一", "标题二数据一", "标题三数据一"],
-    ["标题一数据二", "标题二数据二", "标题三数据二"],
-    ["标题一数据三", "标题二数据三", "标题三数据三"],
-  ];
+  const dataSource = JSON.parse(
+    '[["name","showValue","toAddress","value","toolTipImg"],["北京","北京","江苏","11","https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/720d976d5b5947c187b8f7039126c10e~tplv-k3u1fbpfcp-zoom-crop-mark:3024:3024:3024:1702.awebp?"],["四川","四川","江苏","11","https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/720d976d5b5947c187b8f7039126c10e~tplv-k3u1fbpfcp-zoom-crop-mark:3024:3024:3024:1702.awebp?"],["浙江","浙江","江苏","22","https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/720d976d5b5947c187b8f7039126c10e~tplv-k3u1fbpfcp-zoom-crop-mark:3024:3024:3024:1702.awebp?"],["山东","山东","江苏","22","https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/720d976d5b5947c187b8f7039126c10e~tplv-k3u1fbpfcp-zoom-crop-mark:3024:3024:3024:1702.awebp?"],["福建","福建","江苏","33","https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/720d976d5b5947c187b8f7039126c10e~tplv-k3u1fbpfcp-zoom-crop-mark:3024:3024:3024:1702.awebp?"],["上海","上海","江苏","33","https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/720d976d5b5947c187b8f7039126c10e~tplv-k3u1fbpfcp-zoom-crop-mark:3024:3024:3024:1702.awebp?"],["重庆","重庆","江苏","44","https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/720d976d5b5947c187b8f7039126c10e~tplv-k3u1fbpfcp-zoom-crop-mark:3024:3024:3024:1702.awebp?"],["云南","云南","江苏","44","https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/720d976d5b5947c187b8f7039126c10e~tplv-k3u1fbpfcp-zoom-crop-mark:3024:3024:3024:1702.awebp?"],["江西","江西","江苏","55","https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/720d976d5b5947c187b8f7039126c10e~tplv-k3u1fbpfcp-zoom-crop-mark:3024:3024:3024:1702.awebp?"],["广东","广东","江苏","55","https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/720d976d5b5947c187b8f7039126c10e~tplv-k3u1fbpfcp-zoom-crop-mark:3024:3024:3024:1702.awebp?"],["山西","山西","江苏","66","https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/720d976d5b5947c187b8f7039126c10e~tplv-k3u1fbpfcp-zoom-crop-mark:3024:3024:3024:1702.awebp?"],["黑龙江","黑龙江","江苏","66","https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/720d976d5b5947c187b8f7039126c10e~tplv-k3u1fbpfcp-zoom-crop-mark:3024:3024:3024:1702.awebp?"],["陕西","陕西","江苏","77","https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/720d976d5b5947c187b8f7039126c10e~tplv-k3u1fbpfcp-zoom-crop-mark:3024:3024:3024:1702.awebp?"],["辽宁","辽宁","江苏","77","https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/720d976d5b5947c187b8f7039126c10e~tplv-k3u1fbpfcp-zoom-crop-mark:3024:3024:3024:1702.awebp?"],["海南","海南","江苏","88","https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/720d976d5b5947c187b8f7039126c10e~tplv-k3u1fbpfcp-zoom-crop-mark:3024:3024:3024:1702.awebp?"],["湖南","湖南","江苏","99","https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/720d976d5b5947c187b8f7039126c10e~tplv-k3u1fbpfcp-zoom-crop-mark:3024:3024:3024:1702.awebp?"],["宁夏","宁夏","江苏","99","https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/720d976d5b5947c187b8f7039126c10e~tplv-k3u1fbpfcp-zoom-crop-mark:3024:3024:3024:1702.awebp?"],["河北","河北","江苏","111","https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/720d976d5b5947c187b8f7039126c10e~tplv-k3u1fbpfcp-zoom-crop-mark:3024:3024:3024:1702.awebp?"],["湖北","湖北","江苏","111","https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/720d976d5b5947c187b8f7039126c10e~tplv-k3u1fbpfcp-zoom-crop-mark:3024:3024:3024:1702.awebp?"],["内蒙古","内蒙古","江苏","111","https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/720d976d5b5947c187b8f7039126c10e~tplv-k3u1fbpfcp-zoom-crop-mark:3024:3024:3024:1702.awebp?"],["天津","天津","江苏","111","https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/720d976d5b5947c187b8f7039126c10e~tplv-k3u1fbpfcp-zoom-crop-mark:3024:3024:3024:1702.awebp?"],["贵州","贵州","江苏","111","https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/720d976d5b5947c187b8f7039126c10e~tplv-k3u1fbpfcp-zoom-crop-mark:3024:3024:3024:1702.awebp?"],["甘肃","甘肃","江苏","111","https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/720d976d5b5947c187b8f7039126c10e~tplv-k3u1fbpfcp-zoom-crop-mark:3024:3024:3024:1702.awebp?"],["江苏","江苏","江苏","111","https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/720d976d5b5947c187b8f7039126c10e~tplv-k3u1fbpfcp-zoom-crop-mark:3024:3024:3024:1702.awebp?"],["吉林","吉林","江苏","111","https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/720d976d5b5947c187b8f7039126c10e~tplv-k3u1fbpfcp-zoom-crop-mark:3024:3024:3024:1702.awebp?"],["河南","河南","江苏","111","https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/720d976d5b5947c187b8f7039126c10e~tplv-k3u1fbpfcp-zoom-crop-mark:3024:3024:3024:1702.awebp?"],["青海","青海","江苏","111","https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/720d976d5b5947c187b8f7039126c10e~tplv-k3u1fbpfcp-zoom-crop-mark:3024:3024:3024:1702.awebp?"],["广西","广西","江苏","111","https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/720d976d5b5947c187b8f7039126c10e~tplv-k3u1fbpfcp-zoom-crop-mark:3024:3024:3024:1702.awebp?"],["安徽","安徽","江苏","111","https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/720d976d5b5947c187b8f7039126c10e~tplv-k3u1fbpfcp-zoom-crop-mark:3024:3024:3024:1702.awebp?"],["新疆","新疆","江苏","111","https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/720d976d5b5947c187b8f7039126c10e~tplv-k3u1fbpfcp-zoom-crop-mark:3024:3024:3024:1702.awebp?"],["西藏","西藏","江苏","111","https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/720d976d5b5947c187b8f7039126c10e~tplv-k3u1fbpfcp-zoom-crop-mark:3024:3024:3024:1702.awebp?"],["香港","香港","江苏","123","https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/720d976d5b5947c187b8f7039126c10e~tplv-k3u1fbpfcp-zoom-crop-mark:3024:3024:3024:1702.awebp?"],["台湾","台湾","江苏","23123","https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/720d976d5b5947c187b8f7039126c10e~tplv-k3u1fbpfcp-zoom-crop-mark:3024:3024:3024:1702.awebp?"],["澳门","澳门","江苏","123","https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/720d976d5b5947c187b8f7039126c10e~tplv-k3u1fbpfcp-zoom-crop-mark:3024:3024:3024:1702.awebp?"]]'
+  );
   const options = {
     externalVariables: {
-      headerBGColor: "#000",
+      外层边界线条颜色: "#3AFFFF",
+      外层边边界线条粗细: "3",
+      内层边界线条颜色: "#5DBDF2",
+      内层边界线条粗细: "3",
+      下层边界线条颜色: "#027DE4",
+      地图颜色: "transparent",
+      汇聚点: ["新疆", "江苏", "广东"],
+      汇聚点颜色: "#FB9825",
+      飞线颜色: "#FB9825",
+      toolTip宽度: "138",
+      toolTip高度: "40",
+      toolTip文字颜色: "#FFFFFF",
+      toolTip文字大小: "14",
+      地图文字颜色: "red",
     },
   };
-  ReactDOM.render(
-    <App dataSource={dataSource} options={options} />,
-    document.getElementById("root")
-  );
+  ReactDOM.render(<App dataSource={dataSource} options={options} />, document.getElementById("root"));
 } else {
   if (!window.CUSTOM_PLUGIN) {
     window.CUSTOM_PLUGIN = new Map();
