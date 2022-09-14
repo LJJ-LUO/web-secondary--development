55f710167c3930cfefe212437a1834fbbaa3a77f
jiangsixian <jiangsixian@njsdata.com>
2022-09-14 10:21:24 +0800

    init

--- packages/top-custom-plugin/dumi/.umi/dumi/layout.tsx
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import React from 'react';
-import config from '@@/dumi/config';
-import demos from '@@/dumi/demos';
-import apis from '@@/dumi/apis';
-import Layout from 'C:/dev/web-secondary--development/node_modules/_@umijs_preset-dumi@1.1.45@@umijs/preset-dumi/lib/theme/layout';
-
-export default (props) => <Layout {...props} config={config} demos={demos} apis={apis} />;
