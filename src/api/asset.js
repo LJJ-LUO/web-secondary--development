da3f86c0047f6c267f66c6c054cf827ac217bb74
7DodgerBlue <50253073+151hudu@users.noreply.github.com>
2022-09-19 23:55:17 +0800

    2022.9.14-智慧园区-目录-app-vue-plugin (#248)

--- src/api/asset.js
+++ src/api/asset.js
@@ -6,10 +6,12 @@ import request from "./request";
  */
 export const queryAssetById = id =>
   request.post(`/asset/getAssetData?asset_id=${id}`, []);
+export const queryMenuByAssetId = data =>
+  request.post(`/WisdomPark/queryMenuByAssetId`,data);
 
 /**
  * 无鉴权查询资产
  * @param id 资产ID
  */
-export const queryAssetById = id =>
+export const queryAssetByIdForm = id =>
   request.post(`/form/getAssetData?asset_id=${id}`, []);
--- src/api/request.js
+++ src/api/request.js
@@ -4,7 +4,7 @@ import qs from "querystringify";
 let apiContextPath = "";
 if (process.env.NODE_ENV === "development") {
   document.cookie =
-    "token=eyJhbGciOiJIUzI1NiJ9.eyJsb2dpblRpbWVzdGFtcCI6MTY0NjcyMjI2ODY4NSwidXNlcklkIjoiMTIzNDU2Nzg5MCJ9.F8wr84ha-dW18J9wZOQeTXj55mXTdqKfLBeNlNueoLY";
+    "token=eyJhbGciOiJIUzI1NiJ9.eyJsb2dpblRpbWVzdGFtcCI6MTY2MzI5NDc5MTA1NywidXNlcklkIjoiMTIzNDU2Nzg5MCJ9.BgRZ2peqGkBylZKUCjEtz8e5nMg_CznUNrmCK7-64zA";
   document.cookie =
     "refreshToken=eyJhbGciOiJIUzI1NiJ9.eyJsb2dpblRpbWVzdGFtcCI6MTY0NjcyMjI2ODY4Nn0.TEVE_nopHNZlvSQM_RUZrLcCzkaERiHo8nz0q-ksL3E";
   document.cookie = "username=admin";
